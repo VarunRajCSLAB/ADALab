e37146fc1e459647e941e2e711c2f17a7b1c724a
VarunRajCSLAB <139340360+VarunRajCSLAB@users.noreply.github.com>
2023-08-26 20:40:54 +0530

    lab program 9

--- Prims_kruskals.c
+++ Prims_kruskals.c
@@ -1,4 +1,5 @@
 /* Find the minimum cost spanning tree of given undirected graph using prims and kruskal's algorithm.*/
+//Prim's algorithm:
 #include <stdio.h>
 
 float cost[10][10];
@@ -67,3 +68,70 @@ void prims() {
         sum = sum + min;
     }
 }
+
+//Kruskal's algorithm:
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+
+int i, j, k, a, b, u, v, n, ne = 1;
+int min, mincost = 0, cost[9][9], parent[9];
+
+int find(int);
+int uni(int, int);
+
+void main() {
+    printf("Enter the no. of vertices: ");
+    scanf("%d", &n);
+    
+    printf("Enter the cost of adjacency matrix:\n");
+    for (i = 1; i <= n; i++) {
+        for (j = 1; j <= n; j++) {
+            scanf("%d", &cost[i][j]);
+            if (cost[i][j] == 0)
+                cost[i][j] = 999;
+        }
+    }
+    
+    printf("The edges of Minimum Cost Spanning Tree are\n");
+    
+    while (ne < n) {
+        for (i = 1, min = 999; i <= n; i++) {
+            for (j = 1; j <= n; j++) {
+                if (cost[i][j] < min) {
+                    min = cost[i][j];
+                    a = u = i;
+                    b = v = j;
+                }
+            }
+        }
+        
+        u = find(u);
+        v = find(v);
+        
+        if (uni(u, v)) {
+            printf("%d edge (%d,%d) = %d\n", ne++, a, b, min);
+            mincost += min;
+        }
+        
+        cost[a][b] = cost[b][a] = 999;
+    }
+    
+    printf("\nMinimum cost = %d\n", mincost);
+    getch();
+}
+
+int find(int i) {
+    while (parent[i])
+        i = parent[i];
+    return i;
+}
+
+int uni(int i, int j) {
+    if (i != j) {
+        parent[j] = i;
+        return 1;
+    }
+    return 0;
+}
+
